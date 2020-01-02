fc6542ffa76501d38d722ead4b5c072584502ec0
Mark E. McDermott <mark.elias.mcdermott@gmail.com>
2020-01-01 20:28:29 -0500

    Update

--- disperse_to_envelope.py
+++ disperse_to_envelope.py
@@ -2,47 +2,19 @@ import datetime
 import argparse
 
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                   help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                   const=sum, default=max,
-                   help='sum the integers (default: find the max)')
-
-args = parser.parse_args()
-print(args.accumulate(args.integers))
-
 def main():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                        help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                        const=sum, default=max,
-                        help='sum the integers (default: find the max)')
+    parser = argparse.ArgumentParser(description='Disperse to accounts')
+    parser.add_argument('-t','--transaction-list',required=True,
+                        help='TOML file describing transactions for the dispersement')
+    parser.add_argument('-r','--root-account', required=True,
+                        help='Root account string exactly as displayed in GnuCash')
+    parser.add_argument('-l','--left-over-account', required=True,
+                        help='Account string exactly as displayed in GnuCash which accepts any left-over funds after dispersment.')
+    parser.add_argument('-g','--gnucash-ledger', required=True,
+                        help='GnuCash file to use for this dispersement')
 
-args = parser.parse_args()
-print(args.accumulate(args.integers))
-
-    file = open('DO_NOT_COMMIT_email_parameters.dat','rb')
-    param = pickle.loads(file.read())
-    file.close()
-    smtpserver = smtplib.SMTP(param.host,param.port)
-    smtpserver.ehlo()
-    smtpserver.starttls()
-    smtpserver.ehlo
-    smtpserver.login(param.sender, param.passwd)
-
-
-    msg = MIMEText('Fill me in!')
-    msg['Subject'] = 'GnuCash Envelope Status:' + datetime.datetime.now().strftime('%Y-%m-%d')
-    msg['From'] = param.sender
-    msg['To'] =  ';'.join([str(x) for x in param.receivers]) 
-
-    print(param)
-    print(msg)
-    smtpserver.sendmail(param.sender, param.receivers, msg.as_string())
-    smtpserver.quit()
-
+    args = parser.parse_args()
+    print(args)
 
 if __name__ == "__main__":
     # execute only if run as a script
